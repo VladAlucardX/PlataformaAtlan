32dd4a2848110e1515163c1358ba63f8ddaf80c8
Alucard <alucard@example.com>
2026-09-03 20:14:42 -0600

    style(mas-de-nicaragua): ampliar dimensiones de la tarjeta modal general (maxWidth 1320px, height 670px)

--- src/app/mas-de-nicaragua/page.js
+++ src/app/mas-de-nicaragua/page.js
@@ -675,7 +675,7 @@ export default function MasDeNicaraguaPage() {
           inset: 0,
           zIndex: 9999,
           backgroundColor: "#0A192F",
-          padding: "85px 20px 20px",
+          padding: "70px 20px 20px",
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
@@ -721,12 +721,12 @@ export default function MasDeNicaraguaPage() {
             zIndex: 0
           }} />
 
-          {/* Tarjeta Modal (Restaurada al estilo oscuro original) */}
+          {/* Tarjeta Modal General Ampliada */}
           <div style={{
-            maxWidth: "1160px",
+            maxWidth: "1320px",
             width: "100%",
-            height: "560px",
-            maxHeight: "82vh",
+            height: "670px",
+            maxHeight: "88vh",
             display: "flex",
             flexDirection: "column",
             position: "relative",
